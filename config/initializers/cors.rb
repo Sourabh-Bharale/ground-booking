3fbeaa21009a0419d15092d567b6cdbace2bab97
Sourabh-Bharale <sourabhbharale80@gmail.com>
2024-03-09 14:34:47 +0530

    chore[dev-controllers]: setup cors middleware

---
+++ config/initializers/cors.rb
@@ -0,0 +1,11 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins "http://localhost:4000", "http://127.0.0.1:4000" , "http://localhost:5173" 
+
+      resource "*",
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true,
+        max_age: 86400
+    end
+  end
